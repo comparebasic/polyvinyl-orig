fa4f6f990f51f1d8b26c6314ff6ec2669716d4d1
Wheelspawn <nsage@protonmail.com>
2026-03-30 18:09:45 -0500

    updates

--- example/public/banner.js
+++ example/public/banner.js
@@ -1,61 +1,80 @@
 
-leftcol = document.getElementById("leftcol");
-rightcol = document.getElementById("rightcol");
+let vt323 = new FontFace(
+  "VT323",
+  "url(VT323-Regular.ttf)"
+);
 
-var s_left = "";
-var s_right = "";
+function resize(canvas,ctx) {
+  var dpr = window.devicePixelRatio || 1;
 
-// colors = ["#ff4545", "#fff645", "#45ff48", "#45c7ff"]; // too bright
-colors = ["#5c1717", "#5a5c17", "#175c18", "#17465c"];
-dna = ["A", "C", "G", "T"];
-rna = ["A", "C", "G", "U"];
+  canvas.width = canvas.offsetWidth * dpr;
+  canvas.height = canvas.offsetHeight * dpr;
 
-/*
-// trying to get this working: calculate the height and width of the table,
-// divide by height and width of character to find exactly how many characters you can fit into the table
-
-console.log(leftcol.getBoundingClientRect().width);
-leftcol.innerHTML += `<span style="color:#5c1717">A</span>`;
-
-const span = document.createElement("span");
-span.style.height = "1ch";
-span.style.width = "1ch";
-span.style.position = "fixed";
-document.body.appendChild(span);
-const height = span.getBoundingClientRect().height;
-const width = span.getBoundingClientRect().width;
-document.body.removeChild(span);
-
-
-console.log(leftcol.clientHeight / height);
-console.log(leftcol.clientWidth / width);
-console.log((leftcol.clientHeight / height) * (leftcol.clientWidth / width));
-
-for (let i = 0; i < Math.floor(leftcol.clientWidth / Math.ceil(width)); i++) {
-    r = Math.floor(Math.random() * 4);
-    s_left = `<span style="color:${colors[r]}">${dna[r]}</span>`;
-    leftcol.innerHTML += s_left;
-} */
-
-// too slow for big screens because it recalculates everything thousands of times
-timeout = 0;
-
-leftcol_height = leftcol.clientHeight;
-while (leftcol.clientHeight == leftcol_height && timeout < 10000)
-{
-    r = Math.floor(Math.random() * 4);
-    s_left = `<span style="color:${colors[r]}">${dna[r]}</span>`;
-    leftcol.innerHTML += s_left;
-    timeout += 1;
+  ctx.scale(dpr, dpr);
 }
-leftcol.innerHTML = leftcol.innerHTML.slice(0,-s_left.length);
 
-rightcol_height = rightcol.clientHeight;
-while (rightcol.clientHeight == rightcol_height && timeout < 10000)
-{
-    r = Math.floor(Math.random() * 4);
-    s_right = `<span style="color:${colors[r]}">${rna[r]}</span>`;
-    rightcol.innerHTML += s_right;
-    timeout += 1;
+dna = ["A", "C", "T", "G"];
+colors = ["red", "#989bff", "#f6ff00", "#00ff0d"];
+
+function draw(canvas,ctx) {
+
+  characterSize = 54;
+  ctx.textBaseline = "top";
+  ctx.font = characterSize.toString() + "px VT323";
+
+  paddingHorizontal = 0;
+  paddingVertical = (ctx.measureText('A').actualBoundingBoxAscent + ctx.measureText('A').actualBoundingBoxDescent)/5;
+  
+  characterWidth = ctx.measureText('A').width + paddingHorizontal;
+  characterHeight = ctx.measureText('A').actualBoundingBoxAscent + ctx.measureText('A').actualBoundingBoxDescent + paddingVertical;
+
+  numCols = Math.floor(canvas.offsetWidth / characterWidth);
+  numRows = Math.floor(canvas.offsetHeight / characterHeight);
+
+  cellWidth = canvas.offsetWidth / numCols;
+  cellHeight = canvas.offsetHeight / numRows;
+
+  // subtract the cell width from the character width and divide by 2 to get the offset for centering the text
+  offsetX = (cellWidth - characterWidth)/2;
+  offsetY = (cellHeight - (ctx.measureText('A').actualBoundingBoxAscent + ctx.measureText('A').actualBoundingBoxDescent))/2;
+  
+  // render the letters
+  for (let m = 0; m < numCols; m++) {
+    for (let n = 0; n < numRows; n++) {
+      r = Math.floor(Math.random() * 4);
+      ctx.fillStyle = colors[r];
+      ctx.fillText(dna[r],offsetX+m*(canvas.offsetWidth/numCols),offsetY+n*(canvas.offsetHeight/numRows)+ctx.measureText('A').actualBoundingBoxAscent);
+    }
+  }
+  
+  // scanline effect
+  ctx.fillStyle = "black";
+  for (let n = 0; n < canvas.offsetHeight; n++) {
+    if (n % 3 == 0) {
+        ctx.fillRect(0,n,canvas.offsetWidth,1)
+    }
+  }
 }
-rightcol.innerHTML = rightcol.innerHTML.slice(0,-s_right.length);
+
+const canvas_1 = document.getElementById("leftcanvas");
+const ctx_1 = canvas_1.getContext("2d");
+
+const canvas_2 = document.getElementById("rightcanvas");
+const ctx_2 = canvas_2.getContext("2d");
+
+vt323.load().then((font) => {
+  document.fonts.add(font);
+  console.log("Font loaded");
+  
+  resize(canvas_1, ctx_1);
+  resize(canvas_2, ctx_2);
+
+  ctx_1.fillStyle = "black";
+  ctx_1.fillRect(0, 0, canvas_1.width, canvas_1.height);
+  
+  ctx_2.fillStyle = "black";
+  ctx_2.fillRect(0, 0, canvas_2.width, canvas_2.height);
+  
+  draw(canvas_1, ctx_1);
+  draw(canvas_2, ctx_2);
+});
